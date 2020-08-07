4a1cb3eff6b1792071f3a2f4f19c2d1215795c91
VictorC04 <65841236+VictorC04@users.noreply.github.com>
2020-08-07 19:00:56 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#As the assignment requires, what this function does is give the 
+#inverse of a created matrix. For example, if you create a 2x2 array 
+#containing the values 2 through 5 with makeCacheMatrix, get () will 
+#print that array to the console, getInverse () will print the inverse 
+#of that array, but since its inverse is not yet available, will result in NULL.
 
+makeCacheMatrix <- function(x = matrix()) {
+        invs <- NULL
+        set <- function(y) {
+                x <<- y
+                invs <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) invs <<- inverse
+        getInverse <- function() invs 
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
+#Here create the function that generates the inverse of the array 
+#previously created with makeCacheMatrix. It will save the inverse 
+#of the matrix, so that when requesting getInverse () on it, it will return its inverse.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        invs <- x$getInverse() ## Return a matrix that is the inverse of 'x'
+        if(!is.null(invs)) {
+                message("getting cached data")
+                return(invs)
+        }
+        mat <- x$get()
+        invs <- solve(mat,...)
+        x$setInverse(invs)
+        invs
+} 
